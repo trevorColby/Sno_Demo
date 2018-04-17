ba60fba99dacb3d1551f9036192e2e912f5990ce
caacree <acreechristopher@gmail.com>
2018-04-17 14:09:05 -0400

    linter stuff and attach hydrants even if loaded first

--- src/components/ImportExport.js
+++ src/components/ImportExport.js
@@ -22,56 +22,27 @@ class ImportExport extends React.Component {
     this.state = {
       selectedFiles: null,
       exportType: null,
-    }
+    };
     this.changeFile = this.changeFile.bind(this);
     this.importFile = this.importFile.bind(this);
   }
 
   changeFile(e) {
     this.setState({
-      selectedFiles: e.target.files
-    })
+      selectedFiles: e.target.files,
+    });
   }
 
-  importFile = () =>  {
+  importFile = () => {
     const { selectedFiles } = this.state;
-    const { importKMLClicked, trails } = this.props;
-    const reader = new FileReader();
-    reader.onload = function(event) {
-      try {
-        const kml = new KML().readFeatures(event.target.result)
-        const newTrails = {};
-        const newHydrants = {};
-
-        _.each(kml, (feature, index) => {
-            const type = feature.getGeometry().getType() === 'Polygon' ? 'trail' : 'hydrant';
-            if(type === 'trail'){
-              const trail = processTrail(feature, index);
-              newTrails[trail.get('id')] = trail;
-            } else {
-              const hydrant = processHydrant(feature, index);
-              newHydrants[hydrant.get('id')] = hydrant;
-            }
-        });
-
-        importKMLClicked({
-          trails: Immutable.Map(newTrails),
-          hydrants:  Immutable.Map(newHydrants),
-        })
-        
-      }
-      catch(err) {
-        // Put error wherever we want to display error msgs.
-          console.log(err);
-      }
-    }
+    const { importKMLClicked, trails, hydrants } = this.props;
 
     function processTrail(feature, index) {
       let [name, ...otherThings] = feature.get('description').split(',');
       name = _.words(name).join(' ');
       const id = index + name;
       const coords = feature.getGeometry().getCoordinates()[0];
-      const lonLatCoords = _.map(coords, (pt) => Projection.fromLonLat(pt.slice(0,2)));
+      const lonLatCoords = _.map(coords, pt => Projection.fromLonLat(pt.slice(0,2)));
       feature.getGeometry().setCoordinates([lonLatCoords]);
       feature.setId(`t${id}`);
       return new Trail({ id, name, coords, feature });
@@ -80,16 +51,50 @@ class ImportExport extends React.Component {
     function processHydrant(feature, index) {
       let [trailName, hydrantIndex, name]  = feature.get('description').split(',');
       trailName = _.words(trailName).join(' ');
-      const trailObj = trails.find((t) => t.get('name') === trailName)
-      const trailId = trailObj ? trailObj.get('id') || null;
+      const trailObj = trails.find(t => t.get('name') === trailName);
+      const trailId = trailObj ? trailObj.get('id') : null;
       const id = index + name;
       const geometry = feature.getGeometry().getGeometries()[0];
       feature.setGeometry(geometry);
       const coords = geometry.getCoordinates();
       feature.getGeometry().setCoordinates(Projection.fromLonLat(coords.slice(0,2)));
       feature.setId(`h${id}`);
+      feature.set('trailName', trailName);
       return new Hydrant({ id, name, coords, feature, trail: trailId });
     }
+
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      try {
+        const kml = new KML().readFeatures(event.target.result);
+        const newTrails = {};
+        const newHydrants = {};
+
+        _.each(kml, (feature, index) => {
+          const type = feature.getGeometry().getType() === 'Polygon' ? 'trail' : 'hydrant';
+          if (type === 'trail') {
+            const trail = processTrail(feature, index);
+            newTrails[trail.get('id')] = trail;
+            hydrants
+              .filter(h => h.get('feature').get('trailName') === trail.get('name'))
+              .forEach((h) => {
+                newHydrants[h.get('id')] = h.set('trail', trail.get('id'));
+              });
+          } else {
+            const hydrant = processHydrant(feature, index);
+            newHydrants[hydrant.get('id')] = hydrant;
+          }
+        });
+
+        importKMLClicked({
+          trails: Immutable.Map(newTrails),
+          hydrants:  Immutable.Map(newHydrants),
+        });
+      } catch (err) {
+        // Put error wherever we want to display error msgs.
+        console.log(err);
+      }
+    };
 
     if (selectedFiles) {
       reader.readAsText(selectedFiles[0]);
@@ -133,17 +138,17 @@ class ImportExport extends React.Component {
   // }
 
   render() {
-    let style= {
-      float: 'left'
-    }
+    const style = {
+      float: 'left',
+    };
 
     return (
-        <div style={style}>
-          <input onChange={this.changeFile} type='file' />
-          <Button variant="raised" onClick={this.importFile} > Import </Button>
-          <Button variant="raised"  onClick={this.exportFile} > Export </Button>
-        </div>
-    )
+      <div style={style}>
+        <input onChange={this.changeFile} type="file" />
+        <Button variant="raised" onClick={this.importFile} > Import </Button>
+        <Button variant="raised" onClick={this.exportFile} > Export </Button>
+      </div>
+    );
   }
 }
 
