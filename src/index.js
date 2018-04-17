8508d1a6bf392d927c69f08e87caaf451c6d24c6
caacree <acreechristopher@gmail.com>
2018-04-17 16:26:36 -0400

    got redux hooked up

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import {Provider} from 'react-redux';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import store from './redux/store';
 import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'ol-geocoder/dist/ol-geocoder.css';
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
 registerServiceWorker();
---
+++ src/redux/reducers/TrailReducer.js
@@ -0,0 +1,75 @@
+import _ from 'lodash';
+import Immutable from 'immutable';
+import ActionTypes from '../ActionTypes';
+
+const {
+  DATA_IMPORTED,
+  TRAIL_ADDED,
+  TRAIL_MODIFIED,
+  TRAIL_SELECTED,
+  TRAIL_DELETED,
+} = ActionTypes;
+
+const initialState = {
+  trails: Immutable.Map(),
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case TRAIL_ADDED: {
+      const { trails } = state;
+      const trail = action.data;
+      const newTrails = trails.set(trail.get('id'), trail);
+      return {
+        ...state,
+        trails: newTrails,
+      };
+    }
+    case TRAIL_SELECTED: {
+      // this is actually bad because state shouldnt cause side effects
+      // but this map stuff is a little wack
+      const { prevSelected, selected } = action.data;
+      if (prevSelected && state.trails.get(prevSelected)) {
+        const feature = state.trails.getIn([prevSelected, 'feature']);
+        feature.unset('selected');
+        feature.changed();
+      }
+      if (selected && state.trails.get(selected)) {
+        const feature = state.trails.getIn([selected, 'feature']);
+        feature.set('selected', true);
+        feature.changed();
+      }
+      return state;
+    }
+    case TRAIL_DELETED: {
+      const trailId = action.data;
+      const newTrails = state.trails.delete(trailId);
+      return {
+        ...state,
+        trails: newTrails,
+      };
+    }
+    case TRAIL_MODIFIED: {
+      const { id, editedFields } = action.data;
+      const newTrail = state.trails.get(id)
+        .withMutations((tr) => {
+          _.each(editedFields, (val, key) => tr.set(key, val));
+        });
+      newTrail.get('feature').setProperties(editedFields);
+      newTrail.get('feature').changed();
+      return {
+        ...state,
+        trails: state.trails.set(id, newTrail),
+      };
+    }
+    case DATA_IMPORTED: {
+      const { trails } = action.data;
+      return {
+        ...state,
+        trails: state.trails.merge(trails),
+      }
+    }
+    default:
+      return state;
+  }
+}
