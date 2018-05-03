666e91d70a39e065ec12b80e8d19bdf5c09ff9fc
jolavb <julianb@bu.edu>
2018-05-03 09:55:51 -0400

    remove console.log from reducer

--- src/redux/reducers/HydrantReducer.js
+++ src/redux/reducers/HydrantReducer.js
@@ -39,7 +39,6 @@ export default (state = initialState, action) => {
       const { hydrants } = state;
       const hydrant = action.data;
       const newHydrants = hydrants.set(hydrant.id, hydrant);
-      console.log(hydrant.toJS())
       return {
         ...state,
         hydrants: newHydrants,
