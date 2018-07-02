d606281d1e081b8c3f7d863c7b6ea6aa361c2d1a
jolavb <bartholdjulian@gmail.com>
2018-07-02 13:21:44 -0400

    Success Adding Hydrants from Alternative Source
    Imported markers generated using google earth and fixed issue with
    new material-ui svg Icon.

--- src/components/ImportExport.js
+++ src/components/ImportExport.js
@@ -94,12 +94,20 @@ class ImportExport extends React.Component {
     }
 
    function processHydrant(feature, index) {
-      let [trailName, hydrantIndex, name]  = feature.get('description').split(',');
+    let trailName, hydrantIndex, name, id
+
+     if (feature.get('description')){
+       let [trailName, hydrantIndex, name]  = feature.get('description').split(',');
+        id = index + name;
+     } else {
+        id = index
+        name = index + ""
+     }
+
       const originalTrailName = name
       trailName = _.words(trailName).join(' ');
       const trailObj = trails.find(t => t.get('name') === trailName);
       const trailId = trailObj ? trailObj.get('id') : null;
-      const id = index + name;
       const geometry = feature.getGeometry().getType() === 'Point' ?
         feature.getGeometry() : feature.getGeometry().getGeometries()[0];
       feature.setGeometry(geometry);
@@ -115,6 +123,7 @@ class ImportExport extends React.Component {
     reader.onload = (event) => {
       try {
         const kml = new KML().readFeatures(event.target.result);
+        console.log("readfeatures", kml)
         const newTrails = {};
         const newHydrants = {};
         _.each(kml, (feature, index) => {
@@ -136,6 +145,7 @@ class ImportExport extends React.Component {
             }
           } else {
             const hydrant = processHydrant(feature, index);
+            console.log(hydrant.toJS())
             newHydrants[hydrant.get('id')] = hydrant;
           }
         });
